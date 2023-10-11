871c0b96fce7fb23aedfdf6f43f2f14e6af551ae
Aakash Rathee <apple@SetupUs123RWCJP.bbrouter>
2023-10-11 15:51:53 +0530

    Rate based rule is also added in the waf

--- waf.tf
+++ waf.tf
@@ -7,6 +7,28 @@ resource "aws_wafv2_web_acl" "webacl1" {
     allow {}
   }
 
+  rule {
+    name     = "rule-1"
+    priority = 1
+
+    action {
+      count {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "webacl1-ratebasedrule"
+      sampled_requests_enabled   = false
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "webacl1-metric"
