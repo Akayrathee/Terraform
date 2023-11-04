8923dfdf3b4738e1de6dd33ebc7446028e967c50
Aakash Rathee <apple@Setup-User-JW123RWCJP.local>
2023-11-04 21:53:19 +0530

    Added a waf acl Domo-Demo1-WAF

--- alb.tf
+++ alb.tf
@@ -1,8 +1,6 @@
-
-
 resource "aws_alb" "WAFalb" {
   name = "LoadBalancerforWAF"
   internal = false
   load_balancer_type = "application"
-  subnets = ["subnet-05c317c270a250504","subnet-01c51bf168e1c3c4b"]
+  subnets = ["subnet-0b87f5a41a45f25d0","subnet-0caef868037a71b9b"]
 }
--- waf.tf
+++ waf.tf
@@ -8,26 +8,94 @@ resource "aws_wafv2_web_acl" "webacl1" {
   }
 
 # For evry rule you need to add another rule block
+  # rule {
+  #   name     = "rule-1"
+  #   priority = 1
+
+  #   action {
+  #     count {}
+  #   }
+
+  #   statement {
+  #     rate_based_statement {
+  #       limit              = 10000
+  #       aggregate_key_type = "IP"
+  #     }
+  #   }
+
+  #   visibility_config {
+  #     cloudwatch_metrics_enabled = true
+  #     metric_name                = "webacl1-ratebasedrule"
+  #     sampled_requests_enabled   = false
+  #   }
+  # }
+
+  
   rule {
-    name     = "rule-1"
-    priority = 1
+    name = "DomoDomainAccept"
+    priority = 0
 
     action {
-      count {}
+      block {}
     }
 
     statement {
-      rate_based_statement {
-        limit              = 10000
-        aggregate_key_type = "IP"
+      not_statement {
+        statement {
+          regex_pattern_set_reference_statement {
+            field_to_match {
+              single_header {
+                name = "host"
+              }
+            }
+            arn = aws_wafv2_regex_pattern_set.DomoHostNames.arn
+            text_transformation {
+              type = "NONE"
+              priority = 0
+            }
+          }
+        }
       }
     }
 
     visibility_config {
+      sampled_requests_enabled = false
       cloudwatch_metrics_enabled = true
-      metric_name                = "webacl1-ratebasedrule"
-      sampled_requests_enabled   = false
+      metric_name = "DomoDomainAccept"
+  }
+  }
+
+  rule {
+    name = "AWS-AWSManagedRulesAmazonIpReputationList"
+    priority = 1
+
+    override_action {
+      none {}
     }
+    
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name = "AWSManagedRulesAmazonIpReputationList"
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+          name = "AWSManagedIPReputationList"
+        }
+        rule_action_override {
+          action_to_use {
+            count {}
+          }
+          name = "AWSManagedReconnaissanceList"
+        }
+      }
+    }
+    visibility_config {
+      sampled_requests_enabled = false
+      cloudwatch_metrics_enabled = true
+      metric_name = "AWS-AWSManagedRulesAmazonIpReputationList"
+  }
   }
 
   visibility_config {
